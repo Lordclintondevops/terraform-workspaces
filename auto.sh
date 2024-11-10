d13db4b2ee99264d6406fd756a4177bb38467622
Lordclintondevops <139438345+Lordclintondevops@users.noreply.github.com>
2024-11-10 22:15:26 +0100

    updated the auto.sh file

--- auto.sh
+++ auto.sh
@@ -1,8 +1,8 @@
 terraform workspace show # it shows the available workspaces
 
-terraform workspace new dev # It will create a new workspace called dev and switch to it
+# terraform workspace new dev # It will create a new workspace called dev and switch to it
 
-terraform workspace delete dev # It will delete the workspace envi called dev
+# terraform workspace delete dev # It will delete the workspace envi called dev
 
 terraform workspace list # it will deplay all the available workspaces
 
@@ -10,10 +10,32 @@ terraform workspace select prod # it will switch to the workspace called prod
 
 terraform plan -var-file prod.tfvars
 
+# terraform apply -var-file prod.tfvars -auto-approve
+
+# sleep 120
+
+terraform destroy -var-file prod.tfvars -auto-approve
+
 terraform workspace select dev
 
 terraform plan -var-file dev.tfvars
 
+# terraform apply -var-file dev.tfvars -auto-approve
+
+# sleep 120
+
+terraform destroy -var-file dev.tfvars -auto-approve
+
 terraform workspace select qa
 
 terraform plan -var-file qa.tfvars
+
+# terraform apply -var-file qa.tfvars -auto-approve
+
+# sleep 120
+
+ terraform destroy -var-file qa.tfvars -auto-approve
+
+
+
+
